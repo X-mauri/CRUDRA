a50e15367e5e4203ecabf057f035effc29e71024
mauri <mauri@LAPTOP-16QG19H7>
2024-05-27 08:48:49 -0600

    Agregar archivos de proyecto.

---
+++ CRUDRA/Pages/CRUD.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MP.Master" AutoEventWireup="true" CodeBehind="CRUD.aspx.cs" Inherits="CRUDRA.Pages.CRUD" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="title" runat="server">
+    CRUD
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="body" runat="server">
+    <br />
+    <div class="mx-auto" style="width: 250px">
+        <asp:Label runat="server" CssClass="h2" ID="lbltitulo"></asp:Label>
+    </div>
+    <form runat="server" class="h-100 d-flex align-items-center justify-content-center">
+        <div>
+            <div class="mb-3">
+                <label class="form-label">Nombre</label>
+                <asp:TextBox runat="server" CssClass="form-control" ID="tbnombre"></asp:TextBox>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Apellido Paterno</label>
+                <asp:TextBox runat="server" CssClass="form-control" ID="tbapellidop"></asp:TextBox>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Apellido Materno</label>
+                <asp:TextBox runat="server" CssClass="form-control" ID="tbapellidom"></asp:TextBox>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Numero de control</label>
+                <asp:TextBox runat="server" CssClass="form-control" ID="tbnumeroc"></asp:TextBox>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Carrera</label>
+                <asp:TextBox runat="server" CssClass="form-control" ID="tbcarrera"></asp:TextBox>
+            </div>
+            <asp:Button runat="server" CssClass="btn btn-primary" ID="BtnCreate" Text="Crear" Visible="false" OnClick="BtnCreate_Click" />
+            <asp:Button runat="server" CssClass="btn btn-primary" ID="BtnUpdate" Text="Actualizar" Visible="false" onclick="BtnUpdate_Click"/>
+            <asp:Button runat="server" CssClass="btn btn-primary" ID="BtnDelete" Text="Borrar" Visible="false" OnClick="BtnDelete_Click" />
+            <asp:Button runat="server" CssClass="btn btn-primary btn-dark" ID="BtnVolver" Text="Volver" Visible="True" OnClick="BtnVolver_Click" />
+        </div>
+    </form>
+</asp:Content>
---
+++ CRUDRA/Pages/CRUD.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace CRUDRA.Pages
+{
+
+
+    public partial class CRUD
+    {
+
+        /// <summary>
+        /// Control lbltitulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbltitulo;
+
+        /// <summary>
+        /// Control tbnombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbnombre;
+
+        /// <summary>
+        /// Control tbapellidop.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbapellidop;
+
+        /// <summary>
+        /// Control tbapellidom.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbapellidom;
+
+        /// <summary>
+        /// Control tbnumeroc.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbnumeroc;
+
+        /// <summary>
+        /// Control tbcarrera.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbcarrera;
+
+        /// <summary>
+        /// Control BtnCreate.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnCreate;
+
+        /// <summary>
+        /// Control BtnUpdate.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnUpdate;
+
+        /// <summary>
+        /// Control BtnDelete.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnDelete;
+
+        /// <summary>
+        /// Control BtnVolver.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnVolver;
+    }
+}
